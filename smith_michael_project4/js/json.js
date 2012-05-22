10a558d7df8d21166de871332c65af00dca9515e
mikesmithjr <mikesmithjr@fullsail.edu>
2012-05-21 23:46:38 -0500

    Adding JSON Data and html script tag.

---
+++ smith_michael_project4/js/json.js
@@ -0,0 +1,64 @@
+var json = {
+	"logItem1": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "08:00"],
+		"bsreading": ["Blood Sugar Reading:", "125"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "7"],
+		"treatments": ["Current Treatment:", "Diet and Pills"],
+		"comments":	["Comments:", "Feeling Great Today!!"]
+	},
+
+	"logItem2": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "12:00"],
+		"bsreading": ["Blood Sugar Reading:", "75"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "4"],
+		"treatments": ["Current Treatment:", "Diet and Pills"],
+		"comments":	["Comments:", "Not feeling well."]
+	},
+	
+	"logItem3": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "16:00"],
+		"bsreading": ["Blood Sugar Reading:", "165"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "5"],
+		"treatments": ["Current Treatment:", "Diet and Pills"],
+		"comments":	["Comments:", "Feeling OK."]
+	},
+	
+	"logItem4": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "19:00"],
+		"bsreading": ["Blood Sugar Reading:", "115"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "8"],
+		"treatments": ["Current Treatment:", "Diet and Pills"],
+		"comments":	["Comments:", "Feeling Great!!"]
+	},
+	"logItem5": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/13/2012"],
+		"currentTime": ["Current Time:", "18:00"],
+		"bsreading": ["Blood Sugar Reading:", "125"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "10"],
+		"treatments": ["Current Treatment:", "Diet and Pills"],
+		"comments":	["Comments:", "Feeling Awesome!!"]
+	}
+	
+
+
+}
+
